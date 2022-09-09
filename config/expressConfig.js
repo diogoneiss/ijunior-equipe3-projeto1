217895253b9faeb64e5779f2ed87f5cb9f8f4f6b
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2022-09-09 14:39:04 -0300

    feat: Criacao entidade musica

--- config/expressConfig.js
+++ config/expressConfig.js
@@ -7,4 +7,7 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+const musicaRouter=require('../src/domains/musicas/controllers/index');
+app.use('/api/musica', musicaRouter)
+
 module.exports=app;
---
+++ src/domains/musicas/controllers/index.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router=express.Router();
+
+const Musica=require('../models/Musica');
+
+
+module.exports=router;
