801023a08638116d9cb0d1619a90630e8ce641bc
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2022-09-09 14:29:36 -0300

    feat: criacao .env e pastas adicionais

---
+++ index.js
@@ -0,0 +1,9 @@
+const app=require(`./config/expressConfig`);
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
