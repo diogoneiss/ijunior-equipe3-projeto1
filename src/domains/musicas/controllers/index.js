f5d0015c74bca8d382dea7b1254b81cc6e94c61d
Vinicius Galvao <viniciuspgalvao@hotmail.com>
2022-09-10 12:34:56 -0300

    feat: rota POST para inserir musicas

--- src/domains/musicas/controllers/index.js
+++ src/domains/musicas/controllers/index.js
@@ -12,5 +12,16 @@ router.get('/',(req, res) => {
 
 });
 
+router.post('/',(req,res) => {
+    newMusica={
+    nome:req.body.nome || "",
+    artista: req.body.artista || "",
+    genero: req.body.genero || "",
+    quantidadeDownloads: req.body.quantidadeDownloads || 0.
+    };
+    Musica.append(newMusica);
+
+});
+
 
 module.exports=router;
