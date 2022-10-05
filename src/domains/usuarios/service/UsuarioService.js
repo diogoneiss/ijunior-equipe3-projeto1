f02270946b023e65bc2cddee84a6c16a0431c63c
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2022-10-05 13:41:35 -0300

    feat:criado

--- src/domains/usuarios/service/UsuarioService.js
+++ src/domains/usuarios/service/UsuarioService.js
@@ -1,15 +1,45 @@
+const bcrypt = require('bcrypt');
 const InvalidParametersError = require('../../../../errors/InvalidParametersError');
+const NotAuthorizedError = require('../../../../errors/NotAuthorizedError');
+const userRoles = require('../constants/userRoles');
 const Usuario = require('../models/Usuario');
 
 class UsuarioService {
+
+	//criptografa a senha usando a biblioteca Bcrypt
+	async criptografarSenha(senha){
+		const saltRounds=10;
+		const senhaCriptografada= await bcrypt.hash(senha, saltRounds);
+		return senhaCriptografada;
+	}
+
+
 	//cria um usuario com base no body recebido
-	async criacao(usuario) {
-		const user=await findOne({where: (email=usuario.email)});
+	async criacao(body) {
+		//A criacao de usuarios administradores nao pode ser realizada por qualquer um, deve haver um protocolo para isso
+		if(body.cargo==userRoles.admin){
+			throw new NotAuthorizedError('Nao eh possivel criar um usuario com cargo de admin');
+		}
+		//procura se ja existe um usuario cadastrado com o email requisitado
+		//se nao, cria um objeto usuario e o cria no BD 
+		const user=await Usuario.findOne({where: {email: body.email}});
 		if(user){
 			throw new InvalidParametersError('Email ja cadastrado');
+		}else{
+			const user ={
+				nome: body.nome,
+				email: body.email,
+				senha: body.senha,
+				cargo: body.cargo
+			};
+			user.senha = await this.criptografarSenha(body.senha);
+
+			await Usuario.create(user);
 		}
-		await Usuario.create(usuario);
+		//criptografa a senha antes de manda-la ao banco de dados
+
 	}
+
 
 	//retorna todos os usuarios
 	async listarTodos() {
